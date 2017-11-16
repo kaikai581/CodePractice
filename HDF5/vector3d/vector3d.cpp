3557fdced33aaa2ac833ae148b4114748fef042a
Shih-Kai Lin <shihkailin78@gmail.com>
2017-11-16 09:43:11 -0600

    HDF5 test. Try a makefile which is OS aware.

---
+++ HDF5/vector3d/vector3d.cpp
@@ -0,0 +1,12 @@
+/*
+ * This program tries to write data to an hdf5 file with data in C++ vectors.
+ */
+
+#include "H5Cpp.h"
+
+using namespace H5;
+
+int main(int argc, char* argv[])
+{
+  return 0;
+}
