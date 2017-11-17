6a8b211b6d527f4dd8551324a106ba0bcd80205d
Shih-Kai Lin <shihkailin78@gmail.com>
2017-11-17 10:36:37 -0600

    The last version only work with very small size of data, when the memory incerment does not touch anything in use. Another vector version with a big chunk of pre-reserved memory almost work up to like 600x100x100 data size. This is a fully working one.

--- HDF5/vector3d/vector3d.cpp
+++ HDF5/vector3d/vector3d.cpp
@@ -11,45 +11,41 @@ using namespace std;
 
 const H5std_string FILE_NAME("h5tutr_dset.h5");
 const H5std_string DATASET_NAME("dset");
+const int NN = 4000;
+const int NC = 4;
+const int NH = 100;
+const int NW = 100;
 
 int main(int argc, char* argv[])
 {
-  // Data initialization.
-  vector<vector<vector<unsigned char*> > > data(2, vector<vector<unsigned char*> >(3, vector<unsigned char*>(4, NULL)));
-  // Assign the memory address
-  unsigned char* ctr = new unsigned char();
-  for(int i = 0; i < 2; i++)
-  {
-    for(int j = 0; j < 3; j++)
-    {
-      for(int k = 0; k < 4; k++)
-      {
-        data[i][j][k] = ctr++;
-        *data[i][j][k] = i + j + k;
-        cout << (int)*data[i][j][k] << " ";
+  unsigned char**** A = new unsigned char***[NN];
+  unsigned char***  B = new unsigned char**[NN*NC];
+  unsigned char**   C = new unsigned char*[NN*NC*NH];
+  unsigned char*    D = new unsigned char[NN*NC*NH*NW];
+  for(int i = 0; i < NN; i++) {
+    for(int j = 0; j < NC; j++) {
+      for(int k = 0; k < NH; k++) {
+        C[NH*(NC*i+j)+k] = D+(NH*(NC*i+j)+k)*NW;
       }
-      cout << endl;
+      B[NC*i+j] = C+NH*(NC*i+j);
     }
-    cout << endl;
+    A[i] = B+NC*i;
   }
-  
-  /// Code for simple array.
-  //~ unsigned char data[2][3][4];
-  //~ cout << endl << "data:" << endl << endl;
-  //~ for(int i = 0; i < 2; i++)
-  //~ {
-    //~ for(int j = 0; j < 3; j++)
-    //~ {
-      //~ for(int k = 0; k < 4; k++)
-      //~ {
-        //~ data[i][j][k] = i + j + k;
-        //~ cout << (int)data[i][j][k] << " ";
-      //~ }
+  for(int i = 0; i < NN; i++) {
+    for(int j = 0; j < NC; j++) {
+      for(int k = 0; k < NH; k++) {
+        for(int l = 0; l < NW; l++) {
+          A[i][j][k][l] = i+j+k+l;
+          //~ cout << (int)A[i][j][k][l] << " ";
+        }
+        //~ cout << endl;
+      }
       //~ cout << endl;
-    //~ }
+    }
     //~ cout << endl;
-  //~ }
-  
+  }
+        
+
   try
   {
     // Turn off the auto-printing when failure occurs so that we can
@@ -60,19 +56,19 @@ int main(int argc, char* argv[])
     H5File file(FILE_NAME, H5F_ACC_TRUNC);
     
     // Create the data space for the dataset.
-    hsize_t dims[3];               // dataset dimensions
-    dims[0] = 2;
-    dims[1] = 3;
-    dims[2] = 4;
-    DataSpace dataspace(3, dims);
+    hsize_t dims[4];               // dataset dimensions
+    dims[0] = NN;
+    dims[1] = NC;
+    dims[2] = NH;
+    dims[3] = NW;
+    DataSpace dataspace(4, dims);
     
     // Create the dataset.
     DataSet dataset = file.createDataSet(DATASET_NAME, PredType::STD_U8LE, dataspace);
     
     // Write the data to the dataset using default memory space, file
     // space, and transfer properties.
-    //~ dataset.write(data, PredType::STD_U8LE); // Code for simple array.
-    dataset.write(data[0][0][0], PredType::STD_U8LE);
+    dataset.write(&A[0][0][0][0], PredType::STD_U8LE);
   }
   // catch failure caused by the H5File operations
   catch(FileIException error)
@@ -80,6 +76,6 @@ int main(int argc, char* argv[])
     error.printError();
     return -1;
   }
-  
+
   return 0;
 }
